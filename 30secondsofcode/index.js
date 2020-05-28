980e3687d348ccc7e10c4cc0f9e867b8f36f44f4
Dxxxxxxww <gotu2018@163.com>
2020-05-28 08:36:31 +0800

    修改文件目录，增加 js 深入内容 by dxw

--- 30secondsofcode/index.js
+++ 30secondsofcode/index.js
@@ -10,6 +10,7 @@ const getUrlParams = (url) =>
     ),
     {}
   )
+// '?a=1&b=2'.match(/([^?=&]+)(=([^&]*))/g) ["a=1", "b=2"]
 
 /**
  * @description get base url  window.location 方法里也有属性可以看看
@@ -18,3 +19,27 @@ const getUrlParams = (url) =>
  */
 const getBaseUrl = (url) =>
   url.includes('?') ? url.slice(0, url.indexOf('?')) : url
+
+/**
+ * @description if protocol is not https, redirect to https://xxxx
+ */
+const httpsRedirect = () => {
+  if (location.protocol !== 'https:')
+    location.replace('https://' + location.href.split('//')[1])
+}
+
+/**
+ * @description 传入一个对象，转化成 url 的 param
+ * @param {*} param
+ */
+const objectToQueryString = (param) => {
+  return param
+    ? Object.entries(param).reduce((queryString, [key, value]) => {
+        const symbol = queryString.length === 0 ? '?' : '&'
+        queryString +=
+          typeof value === 'string' ? `${symbol}${key}=${value}` : ''
+        return queryString
+      }, '')
+    : ''
+}
+objectToQueryString({ page: '1', size: '2kg', key: undefined }) // ?page=1&size=2kg
